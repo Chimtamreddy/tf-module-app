e0897637b5e66e33763d9a8fd84e475dccdac55c
Chimtamreddy <kr7348202@gmail.com>
2024-04-19 10:09:40 +0530

    roboshop terraform

--- vars.tf
+++ vars.tf
@@ -7,4 +7,10 @@ variable "port" {}
 variable "sg_ingress_cidr" {}
 variable "ssh_ingress_cidr" {}
 variable "zone_id" {}
+variable "instance_type" {}
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
+
+
 
