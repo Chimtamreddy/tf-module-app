764dc0740f6e5ee8ed1e06812769e46c8e942047
Chimtamreddy <kr7348202@gmail.com>
2024-04-28 11:27:14 +0530

    roboshop terraform

--- main.tf
+++ main.tf
@@ -40,6 +40,7 @@ resource "aws_security_group" "main" {
   }
 }
 resource "aws_security_group_rule" "nginx_exporter" {
+  count = var.component == "frontend" ? 1 : 0
   type              = "ingress"
   from_port         = 9113
   to_port           = 9113
